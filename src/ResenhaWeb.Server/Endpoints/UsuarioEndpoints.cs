2d30f3887636d195ac63faed5e08c4ec27333d47
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2026-09-13 09:53:01 -0300

    Ajuste

--- src/ResenhaWeb.Server/Endpoints/UsuarioEndpoints.cs
+++ src/ResenhaWeb.Server/Endpoints/UsuarioEndpoints.cs
@@ -1,25 +1,24 @@
 ﻿using Resenha.Modulo.Usuario.Application.DTOs;
 using Resenha.Modulo.Usuario.Application.Interfaces;
 
-namespace ResenhaWeb.Endpoints
+namespace ResenhaWeb.Endpoints;
+
+public static class UsuarioEndpoints
 {
-    public static class UsuarioEndpoints
+    public static IEndpointRouteBuilder MapUsuarioEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        public static IEndpointRouteBuilder MapUsuarioEndpoints(this IEndpointRouteBuilder endpoints)
-        {
-            endpoints.MapPost("/api/usuarios/", async (RegistrarUsuarioDto dto, IUsuarioAppService usuarioAppService) =>
-             {
-                 var (sucesso, erros) = await usuarioAppService.RegistrarAsync(dto);
+        endpoints.MapPost("/api/usuarios/", async (RegistrarUsuarioDto dto, IUsuarioAppService usuarioAppService) =>
+         {
+             var (sucesso, erros) = await usuarioAppService.RegistrarAsync(dto);
 
-                 if (!sucesso)
-                     return Results.BadRequest(new { Erros = erros });
+             if (!sucesso)
+                 return Results.BadRequest(new { Erros = erros });
 
-                 return Results.Ok(new { Mensagem = "Usuário registrado com sucesso." });
-             })
-             .WithName("RegistrarUsuario")
-             .WithTags("Usuarios");
+             return Results.Ok(new { Mensagem = "Usuário registrado com sucesso." });
+         })
+         .WithName("RegistrarUsuario")
+         .WithTags("Usuarios");
 
-            return endpoints;
-        }
+        return endpoints;
     }
 }
