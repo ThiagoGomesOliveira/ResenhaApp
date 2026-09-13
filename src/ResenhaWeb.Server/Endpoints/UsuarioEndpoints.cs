17b363fe80c3e26e4c5a3a452d95f2c3cb9d74ea
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2026-09-13 09:51:27 -0300

    feat(Web): Criacao usuario endpoint

---
+++ src/ResenhaWeb.Server/Endpoints/UsuarioEndpoints.cs
@@ -0,0 +1,25 @@
+﻿using Resenha.Modulo.Usuario.Application.DTOs;
+using Resenha.Modulo.Usuario.Application.Interfaces;
+
+namespace ResenhaWeb.Endpoints
+{
+    public static class UsuarioEndpoints
+    {
+        public static IEndpointRouteBuilder MapUsuarioEndpoints(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapPost("/api/usuarios/", async (RegistrarUsuarioDto dto, IUsuarioAppService usuarioAppService) =>
+             {
+                 var (sucesso, erros) = await usuarioAppService.RegistrarAsync(dto);
+
+                 if (!sucesso)
+                     return Results.BadRequest(new { Erros = erros });
+
+                 return Results.Ok(new { Mensagem = "Usuário registrado com sucesso." });
+             })
+             .WithName("RegistrarUsuario")
+             .WithTags("Usuarios");
+
+            return endpoints;
+        }
+    }
+}
